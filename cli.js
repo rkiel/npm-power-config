32fa9fbd7fe3fabe8c5efd6d4152a501e07a73ac
rkiel <cygnus@pobox.com>
2018-12-06 08:02:16 -0500

    basics: added commander and lodash

--- cli.js
+++ cli.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log(`Hello World ${process.argv}`);
+const _ = require('lodash');
+const fp = require('lodash/fp');
+const program = require('commander');
+
+function actionHandler() {
+  const actual = _.filter(_.split(program.example, '.'), x => x !== 'example').join('.');
+  console.log(program.example, actual);
+}
+
+program
+  .version('0.0.1')
+  .option('-e, --example <example>', 'The example')
+  .action(actionHandler)
+  .parse(process.argv);
