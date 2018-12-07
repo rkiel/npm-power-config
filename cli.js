8b9364c253ddb6db3083b92bf5fea973f8c8ba3b
rkiel <cygnus@pobox.com>
2018-12-07 10:57:49 -0500

    basics: added personal file

--- cli.js
+++ cli.js
@@ -11,9 +11,29 @@ const writeYamlFile = require('./lib/writeYamlFile');
 
 const fs = require('fs');
 
-function addCommandLineArguments(data) {
-  data.exampleFileName = program.example;
-  data.environment = program.environment;
+function addEnvironment(data) {
+  const environment = _.get(data, 'program.environment');
+  if (!_.isUndefined(environment)) {
+    data.environment = environment;
+  }
+  return data;
+}
+
+function addExampleFileName(data) {
+  data.exampleFileName = _.get(data, 'program.example');
+  return data;
+}
+
+function addActualFileName(data) {
+  const parts = _.split(data.exampleFileName, '.');
+  const doesNotContainExampleWord = x => !_.includes(['example', 'sample'], x);
+  const lessParts = _.filter(parts, doesNotContainExampleWord);
+  data.actualFileName = lessParts.join('.');
+  return data;
+}
+
+function addPersonalFileName(data) {
+  data.personalFileName = `.${data.actualFileName}`;
   return data;
 }
 
@@ -28,60 +48,88 @@ function addFileFormat(data) {
   return data;
 }
 
-function addActualFileName(data) {
-  const parts = _.split(data.exampleFileName, '.');
-  const doesNotContainExampleWord = x => !_.includes(['example', 'sample'], x);
-  const lessParts = _.filter(parts, doesNotContainExampleWord);
-  data.actualFileName = lessParts.join('.');
-  return data;
-}
-
 function readExampleFile(data) {
   switch (data.format) {
     case 'json':
-      data.json = parseJsonFile(data.exampleFileName);
+      data.exampleJson = parseJsonFile(data.exampleFileName);
       break;
     case 'yaml':
-      data.json = parseYamlFile(data.exampleFileName);
+      data.exampleJson = parseYamlFile(data.exampleFileName);
       break;
     default:
-      data.json = {};
+      data.exampleJson = {};
+  }
+  return data;
+}
+
+function readPersonalFile(data) {
+  if (fs.existsSync(data.personalFileName)) {
+    switch (data.format) {
+      case 'json':
+        data.personalJson = parseJsonFile(data.personalFileName);
+        break;
+      case 'yaml':
+        data.personalJson = parseYamlFile(data.personalFileName);
+        break;
+      default:
+        data.personalJson = {};
+    }
+  } else {
+    data.personalJson = {};
   }
   return data;
 }
 
 function generateOutput(data) {
-  data.output = data.json;
+  data.actualJson = data.exampleJson;
   return data;
 }
 
 function writeActualFile(data) {
   switch (data.format) {
     case 'json':
-      writeJsonFile(data.actualFileName, data.output);
+      writeJsonFile(data.actualFileName, data.actualJson);
       break;
     case 'yaml':
-      writeYamlFile(data.actualFileName, data.output);
+      writeYamlFile(data.actualFileName, data.actualJson);
       break;
   }
   return data;
 }
 
-function actionHandler() {
-  const f = [
-    addCommandLineArguments,
-    addActualFileName,
-    addFileFormat,
-    readExampleFile,
-    generateOutput,
-    writeActualFile
-  ];
-  _.flow(f)({});
+function writePersonalFile(data) {
+  switch (data.format) {
+    case 'json':
+      writeJsonFile(data.personalFileName, data.personalJson);
+      break;
+    case 'yaml':
+      writeYamlFile(data.personalFileName, data.personalJson);
+      break;
+  }
+  return data;
+}
+
+function actionHandler(p) {
+  return function() {
+    const f = [
+      addEnvironment,
+      addExampleFileName,
+      addActualFileName,
+      addPersonalFileName,
+      addFileFormat,
+      readExampleFile,
+      readPersonalFile,
+      generateOutput,
+      writePersonalFile,
+      writeActualFile
+    ];
+    _.flow(f)({ program: p });
+  };
 }
 
 program
   .version('0.0.1')
   .option('-x, --example <example>', 'The example configuration file')
   .option('-e, --environment <environment>', 'The environment like dev, test, prod')
-  .action(actionHandler)
+  .action(actionHandler(program))
   .parse(process.argv);
