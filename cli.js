9779bf50d40a08b3af6b47bd78663b82c7c04657
rkiel <cygnus@pobox.com>
2019-07-11 22:01:03 -0400

    basics: added review based on a pattern

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const _ = require('lodash');
-const program = require('commander');
+const _ = require("lodash");
+const program = require("commander");
 
 function actionHandler(p) {
   return function() {
@@ -20,30 +20,50 @@ function actionHandler(p) {
       lib._output.writeFile
     ];
     _.flow(f)({
-      program: _.pick(p, ['input', 'output', 'example', 'environment', 'flatten', 'clear', 'userInputs', 'review'])
+      program: _.pick(p, [
+        "input",
+        "output",
+        "example",
+        "environment",
+        "flatten",
+        "clear",
+        "userInputs",
+        "review",
+        "pattern"
+      ])
     });
   };
 }
 
 lib = {
-  _rc: require('./lib/rc'),
-  _environment: require('./lib/environment'),
-  _example: require('./lib/example'),
-  _output: require('./lib/output'),
-  _input: require('./lib/input'),
-  _transform: require('./lib/transform'),
+  _rc: require("./lib/rc"),
+  _environment: require("./lib/environment"),
+  _example: require("./lib/example"),
+  _output: require("./lib/output"),
+  _input: require("./lib/input"),
+  _transform: require("./lib/transform"),
   actionHandler
 };
 
 program
-  .version('0.2.0')
-  .option('-i, --input <input>', 'input configuration file')
-  .option('-o, --output <output>', 'output configuration file')
-  .option('-x, --example <example>', `example configuration file. Default is ${lib._example.DEFAULT}`)
-  .option('-e, --environment <environment>', 'environment such as dev, test, or prod')
-  .option('-f, --flatten', 'flatten nested environment')
-  .option('-C, --clear <clear>', 'path(s) to input value(s) to clear and/or set')
-  .option('-U, --no-user-inputs', 'exit with failure if user input is required')
-  .option('-R, --review', 'review all user inputs')
+  .version("0.2.0")
+  .option("-i, --input <input>", "input configuration file")
+  .option("-o, --output <output>", "output configuration file")
+  .option(
+    "-x, --example <example>",
+    `example configuration file. Default is ${lib._example.DEFAULT}`
+  )
+  .option(
+    "-e, --environment <environment>",
+    "environment such as dev, test, or prod"
+  )
+  .option("-f, --flatten", "flatten nested environment")
+  .option(
+    "-C, --clear <clear>",
+    "path(s) to input value(s) to clear and/or set"
+  )
+  .option("-U, --no-user-inputs", "exit with failure if user input is required")
+  .option("-R, --review", "review all user inputs")
+  .option("-p, --pattern <pattern>", "review user inputs that match pattern")
   .action(lib.actionHandler(program))
   .parse(process.argv);
